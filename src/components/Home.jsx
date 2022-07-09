afd1aa50896c38097855b5f49da3977e22324a1e
吟 <OTO@OTO-MacBook-Pro.local>
2022-07-09 19:10:04 +0900

    comment

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,12 +4,12 @@ import { Text } from "@nextui-org/react";
 import { Card } from "@nextui-org/react";
 import { Button, Spacer } from "@nextui-org/react";
 import { Head } from "./Head";
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Ycontext } from "./context/Ycontext";
 import { useNavigate } from "react-router-dom";
 import { Footer } from "./Footer";
 import { ref, getDownloadURL } from "firebase/storage";
-import { useState } from "react";
+
 import { collection, setDoc, getDoc, getDocs } from "firebase/firestore";
 import { doc } from "firebase/firestore";
 
@@ -17,8 +17,12 @@ export const Home = () => {
   const { Background, storage, db } = useContext(Ycontext);
 
   //プロジェクトのディスクリプション用のパス
-  const projectRef = doc(db, `project/project1`);
-  //タイトルのステート
+  const projectRef = collection(db, `project`);
+
+  const [projectDocs, setProjectDocs] = useState();
+  const [isInitProjectData, setisInitProjectData] = useState(false);
+
+  /*//タイトルのステート
   const [Title1, setTitle1] = useState();
 
   //ディスクリプションのステート
@@ -30,7 +34,7 @@ export const Home = () => {
   //参加条件ステート
   const [EntryTerms, setEntryTerms] = useState();
   //デッドラインステート
-  const [Deadline, setDeadline] = useState();
+  const [Deadline, setDeadline] = useState();*/
 
   //useNavigateの設定
   const Navi = useNavigate();
@@ -38,7 +42,7 @@ export const Home = () => {
   //Homeの画像をステート管理→firebaseから持ってくる
   const [HomeImage, setHomeImage] = useState();
   //プロジェクトの小出しイメージのステート管理
-  const [ProjectImage1, setProjectImage1] = useState();
+  const [ProjectImages, setProjectImage] = useState();
 
   useEffect(() => {
     //ホームイメージのダウンロード
@@ -47,30 +51,156 @@ export const Home = () => {
     ).then((url) => {
       setHomeImage(url);
     });
-    //小出しイメージのダウンロード
-    getDownloadURL(
-      ref(
-        storage,
-        `gs://kurogo-f196b.appspot.com/Projects/project1/short/sample1.jpg`
-      )
-    ).then((url) => {
-      setProjectImage1(url);
-    });
-    getDoc(projectRef).then((docSnap) => {
-      const Title11 = docSnap.data().FirstTitle;
-      setTitle1(Title11);
-      const Description111 = docSnap.data().Description1;
-      setDescription11(Description111);
-      const Description222 = docSnap.data().Description2;
-      setDescription22(Description222);
-      const EntryQualification11 = docSnap.data().EntryQualification;
-      setEntryQualification(EntryQualification11);
-      const EntryTerms11 = docSnap.data().EntryTerms;
-      setEntryTerms(EntryTerms11);
-      const Deadline1 = docSnap.data().DeadLine;
-      setDeadline(Deadline1);
+
+    getDocs(projectRef).then((data) => {
+      setProjectDocs(data.docs);
+      setisInitProjectData(true);
     });
   }, []);
+  const DownloadShortImage = () => {
+    for (let i = 0; i < projectDocs.length; i++) {
+      getDownloadURL(
+        ref(
+          storage,
+          `gs://kurogo-f196b.appspot.com/Projects/project${i}/short/sample1.jpg`
+        )
+      ).then((url) => {
+        setProjectImage(url);
+      });
+    }
+  };
+
+  const ProjectList = () => {
+    let List = [];
+    let ImageList = [];
+    if (isInitProjectData) {
+      for (let i = 0; i < projectDocs.length; i++) {
+        getDownloadURL(
+          ref(
+            storage,
+            `gs://kurogo-f196b.appspot.com/Projects/project${
+              i + 1
+            }/short/sample1.jpg`
+          )
+        ).then((url) => {
+          ImageList.push(url);
+          setProjectImage(url);
+        });
+        const doc = projectDocs[i].data();
+        const Images = ImageList[i];
+
+        List.push(
+          <>
+            <Card
+              clickable
+              hoverable
+              css={{
+                height: "250px",
+                width: "1000px",
+                marginLeft: "100px",
+                marginRight: "100px",
+                marginTop: "10px",
+                boxShadow: "4px 4px 20px",
+              }}
+              onClick={() => {
+                Navi("/Introduce");
+              }}
+            >
+              <div style={{ display: "flex" }}>
+                <div
+                  style={{
+                    marginTop: "40px",
+                    marginLeft: "30px",
+                  }}
+                >
+                  <div style={{ width: "150px", height: "150px" }}>
+                    <img
+                      style={{ width: "150px", height: "150px" }}
+                      src={Images}
+                    />
+                  </div>
+                </div>
+                <div style={{ marginTop: "0px" }}>
+                  <div>
+                    <Text
+                      h1
+                      size={25}
+                      css={{
+                        textGradient: "45deg, $blue500 -20%, $pink500 50%",
+                        marginLeft: "50px",
+                        width: "500px",
+                      }}
+                      color="#ff4ecd"
+                      weight="bold"
+                    >
+                      ＜＜{doc.FirstTitle}＞＞
+                    </Text>
+                    <Spacer y={1} />
+                  </div>
+                  <div>
+                    <Text
+                      h1
+                      size={20}
+                      css={{
+                        marginLeft: "30px",
+                      }}
+                      color="white"
+                      weight="bold"
+                    >
+                      {doc.Description1}
+                    </Text>
+                    <Text
+                      h1
+                      size={20}
+                      css={{
+                        marginLeft: "30px",
+                      }}
+                      color="white"
+                      weight="bold"
+                    >
+                      {doc.Description2}
+                    </Text>
+                    <Spacer y={1} />
+                  </div>
+                  <div>
+                    <Text
+                      h1
+                      size={20}
+                      css={{
+                        marginLeft: "30px",
+                        marginTop: "20px",
+                      }}
+                      color="white"
+                      weight="bold"
+                    >
+                      参加要件：
+                      <span style={{ color: "red" }}>
+                        {doc.EntryQualification}
+                      </span>
+                    </Text>
+                  </div>
+                </div>
+                <div style={{ marginTop: "170px", marginLeft: "-100px" }}>
+                  <Button
+                    bordered
+                    color="gradient"
+                    auto
+                    onClick={() => {
+                      Navi("/Introduce");
+                    }}
+                  >
+                    プロジェクト詳細へ
+                  </Button>
+                </div>
+              </div>
+            </Card>
+            <Spacer y={1} />
+          </>
+        );
+      }
+      return <ul>{List}</ul>;
+    }
+  };
   return (
     <>
       <NextUIProvider theme={Background}>
@@ -133,110 +263,7 @@ export const Home = () => {
               </div>
             </div>
             <div>
-              <Card
-                clickable
-                hoverable
-                css={{
-                  height: "250px",
-                  width: "1000px",
-                  marginLeft: "100px",
-                  marginRight: "100px",
-                  marginTop: "10px",
-                  boxShadow: "4px 4px 20px",
-                }}
-                onClick={() => {
-                  Navi("/Introduce");
-                }}
-              >
-                <div style={{ display: "flex" }}>
-                  <div
-                    style={{
-                      marginTop: "40px",
-                      marginLeft: "30px",
-                    }}
-                  >
-                    <div style={{ width: "150px", height: "150px" }}>
-                      <img
-                        style={{ width: "150px", height: "150px" }}
-                        src={ProjectImage1}
-                      />
-                    </div>
-                  </div>
-                  <div style={{ marginTop: "0px" }}>
-                    <div>
-                      <Text
-                        h1
-                        size={25}
-                        css={{
-                          textGradient: "45deg, $blue500 -20%, $pink500 50%",
-                          marginLeft: "50px",
-                          width: "500px",
-                        }}
-                        color="#ff4ecd"
-                        weight="bold"
-                      >
-                        ＜＜{Title1}＞＞
-                      </Text>
-                      <Spacer y={1} />
-                    </div>
-                    <div>
-                      <Text
-                        h1
-                        size={20}
-                        css={{
-                          marginLeft: "30px",
-                        }}
-                        color="white"
-                        weight="bold"
-                      >
-                        {Description11}
-                      </Text>
-                      <Text
-                        h1
-                        size={20}
-                        css={{
-                          marginLeft: "30px",
-                        }}
-                        color="white"
-                        weight="bold"
-                      >
-                        {Description22}
-                      </Text>
-                      <Spacer y={1} />
-                    </div>
-
-                    <div>
-                      <Text
-                        h1
-                        size={20}
-                        css={{
-                          marginLeft: "30px",
-                          marginTop: "20px",
-                        }}
-                        color="white"
-                        weight="bold"
-                      >
-                        参加要件：
-                        <span style={{ color: "red" }}>
-                          {EntryQualification}
-                        </span>
-                      </Text>
-                    </div>
-                  </div>
-                  <div style={{ marginTop: "170px", marginLeft: "-100px" }}>
-                    <Button
-                      bordered
-                      color="gradient"
-                      auto
-                      onClick={() => {
-                        Navi("/Introduce");
-                      }}
-                    >
-                      プロジェクト詳細へ
-                    </Button>
-                  </div>
-                </div>
-              </Card>
+              {ProjectList()}
               <Spacer y={2} />
             </div>
           </div>
